b7bf0d8a2f6f61e4d094ff2f88919dc48c6cf0be
be3short <beshort@ucsc.edu>
2018-02-22 02:32:01 -0800

    Initial commit with project and math shortcut tools

---
+++ src/main/java/edu/ucsc/hsl/hse/tools/math/shortcuts/MathShortcuts.java
@@ -0,0 +1,35 @@
+package edu.ucsc.hsl.hse.tools.math.shortcuts;
+
+public class MathShortcuts
+{
+
+	public Double s(Double val)
+	{
+		return Math.sin(val);
+	}
+
+	public Double c(Double val)
+	{
+		return Math.cos(val);
+	}
+
+	public Double sin(Double val)
+	{
+		return Math.sin(val);
+	}
+
+	public Double cos(Double val)
+	{
+		return Math.cos(val);
+	}
+
+	public Double sq(Double val)
+	{
+		return Math.pow(val, 2);
+	}
+
+	public Double cu(Double val)
+	{
+		return Math.pow(val, 3);
+	}
+}
